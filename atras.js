908c31f598192e23a184d14b2f770691c36113ab
Juampa-DePaulis <12juanpi@gmail.com>
2022-06-08 15:47:29 -0300

    parte atras

---
+++ atras.js
@@ -0,0 +1,14 @@
+const pagos = [];
+const usuarios = [];
+const listado = document.getElementById("list-group");
+const cuadroResultado = document.getElementById("total");
+const usuario = document.getElementById("nombre");
+const pago = document.getElementById("pago");
+
+function agregar() {
+  agregarGastosAListas();
+  ultimoAPantalla();
+  mostrarPagoIndvidualEnHTML();
+}
+
+function agregarGastosAListas() {}
